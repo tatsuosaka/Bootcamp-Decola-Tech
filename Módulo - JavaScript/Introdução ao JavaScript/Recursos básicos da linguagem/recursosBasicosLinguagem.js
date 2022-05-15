2e632b681ab0e62c774d08f17ea3589ebb48dec3
tatsuosaka <matheus.tats@gmail.com>
2022-05-15 17:26:19 -0300

    recursosBasicosLinguagem

---
+++ Módulo - JavaScript/Introdução ao JavaScript/Recursos básicos da linguagem/recursosBasicosLinguagem.js
@@ -0,0 +1,9 @@
+// Manipulando um arquivo/Funções
+
+//() é para declarar atributos por exemplo (a, b)
+
+function soma(a, b) { // Declarar função
+    return a + b;
+}
+
+soma(3, 5); // Chamar função 
